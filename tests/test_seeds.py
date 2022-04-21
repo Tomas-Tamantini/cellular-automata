1a0a99c7d185abb4534c290631cc4a84709caaef
Tomas Tamantini <tomasbft@gmail.com>
2022-04-21 08:55:00 -0300

    Test simple Seeds configurations

--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -2,10 +2,21 @@ from models.rules import Seeds
 
 
 def test_seeds_has_only_two_states():
-    assert Seeds().number_of_cell_states == 2
+    assert Seeds(cells=set()).number_of_cell_states == 2
+
+
+def test_world_has_initial_living_cells():
+    seeds = Seeds(cells={(0, 0), (10, 20)})
+    assert len(seeds.alive_cells) == 2
 
 
 def test_dead_world_stays_dead():
-    seeds = Seeds()
+    seeds = Seeds(cells=set())
+    seeds.tick()
+    assert len(seeds.alive_cells) == 0
+
+
+def test_alive_cells_die():
+    seeds = Seeds(cells={(0, 0), (10, 20)})
     seeds.tick()
     assert len(seeds.alive_cells) == 0
