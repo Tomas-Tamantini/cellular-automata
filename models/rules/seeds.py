e130aeff75e1e8cedc752804fd5f1137f0ed0fda
Tomas Tamantini <tomasbft@gmail.com>
2022-04-21 09:10:50 -0300

    Implement full seeds rule

--- models/rules/seeds.py
+++ models/rules/seeds.py
@@ -1,9 +1,11 @@
 from models.cell import Cell
 
+from .rules_utils import neighbors
+
 
 class Seeds:
-    def __init__(self, cells: set[tuple[int, int]]) -> None:
-        self.__cells = cells
+    def __init__(self, initial_cell_positions: set[tuple[int, int]]) -> None:
+        self.__cells_pos = initial_cell_positions
 
     @property
     def number_of_cell_states(self) -> int:
@@ -11,7 +13,13 @@ class Seeds:
 
     @property
     def alive_cells(self) -> set[Cell]:
-        return self.__cells
+        return {Cell(*p) for p in self.__cells_pos}
 
     def tick(self) -> None:
-        self.__cells = set()
+        num_neighbors = dict()
+        for cell_pos in self.__cells_pos:
+            for neighbor in neighbors(*cell_pos):
+                if neighbor in self.__cells_pos:
+                    continue
+                num_neighbors[neighbor] = num_neighbors.get(neighbor, 0) + 1
+        self.__cells_pos = {p for (p, n) in num_neighbors.items() if n == 2}
