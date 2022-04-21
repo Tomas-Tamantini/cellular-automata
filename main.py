30698059a9eb1c9776177c355c49d0422f8975ca
Tomas Tamantini <tomasbft@gmail.com>
2022-04-21 14:03:20 -0300

    Run wireworld animation example

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from models.rules import Seeds
-from ui import Lattice, run_tkinter_animation
+from models.rules import Seeds, WireWorld
+from ui import run_wireworld_example, run_seeds_example
 
-world = Seeds(
-    {(50, 50), (50, 51), (50, 52), (60, 60), (60, 61), (31, 60), (31, 59), (30, 60), (10, 10), (11, 10)}
-)
-lattice = Lattice(0, 0, 100, 100, cell_size_in_pixels=5)
-
-run_tkinter_animation(world, lattice, frames_per_second=8, num_frames=80)
+if __name__ == "__main__":
+    run_seeds_example()
+    run_wireworld_example()
