c7a73e2d1d45a58afaba871449c3d9ffa7f56049
Tomas Tamantini <tomasbft@gmail.com>
2022-04-21 08:54:45 -0300

    Initialize Seeds with first alive cells

--- models/rules/seeds.py
+++ models/rules/seeds.py
@@ -2,13 +2,16 @@ from models.cell import Cell
 
 
 class Seeds:
+    def __init__(self, cells: set[tuple[int, int]]) -> None:
+        self.__cells = cells
+
     @property
     def number_of_cell_states(self) -> int:
         return 2
 
     @property
     def alive_cells(self) -> set[Cell]:
-        return set()
+        return self.__cells
 
     def tick(self) -> None:
-        pass
+        self.__cells = set()
