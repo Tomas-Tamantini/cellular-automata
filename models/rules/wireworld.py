87804268a9e1f488c18c2c2d30861bf2fb4b5cda
Tomas Tamantini <tomasbft@gmail.com>
2022-04-21 13:53:22 -0300

    Finish implementing wireworld rules

--- models/rules/wireworld.py
+++ models/rules/wireworld.py
@@ -4,25 +4,53 @@ from .rules_utils import neighbors
 
 
 class WireWorld:
-    """
-    States:
-        0 - empty
-        1 - conductor
-        2 - electron head
-        3 - electron tail
-    """
-
-    def __init__(self, initial_cells: set[Cell]) -> None:
-        self.__electron_heads = {cell for cell in initial_cells if cell.state == 2}
-        self.__electron_tails = {cell for cell in initial_cells if cell.state == 3}
+    def __init__(
+        self,
+        conductor_positions: set[tuple[int, int]] = None,
+        electron_head_positions: set[tuple[int, int]] = None,
+        electron_tail_positions: set[tuple[int, int]] = None,
+    ):
+        c = conductor_positions or set()
+        eh = electron_head_positions or set()
+        et = electron_tail_positions or set()
+        if c.intersection(eh) or c.intersection(et) or eh.intersection(et):
+            raise ValueError("Cells cannot overlap")
+        self.__conductor_positions = c
+        self.__electron_head_positions = eh
+        self.__electron_tail_positions = et
 
     @property
     def alive_cells(self) -> set[Cell]:
-        return set.union(self.__electron_heads, self.__electron_tails)
+        """
+        States:
+            0 - empty
+            1 - conductor
+            2 - electron head
+            3 - electron tail
+        """
+        cells = set()
+        for x, y in self.__conductor_positions:
+            cells.add(Cell(x, y, state=1))
+        for x, y in self.__electron_head_positions:
+            cells.add(Cell(x, y, state=2))
+        for x, y in self.__electron_tail_positions:
+            cells.add(Cell(x, y, state=3))
+        return cells
 
     def tick(self) -> None:
-        new_tails = {Cell(c.x, c.y, state=3) for c in self.__electron_heads}
-        self.__electron_tails = new_tails
-
+        new_tails = {c for c in self.__electron_head_positions}
+        new_conductors = {c for c in self.__electron_tail_positions}
         new_heads = set()
-        self.__electron_heads = new_heads
+
+        for c in self.__conductor_positions:
+            num_neighboring_electrons = len(
+                [1 for n in neighbors(*c) if n in self.__electron_head_positions]
+            )
+            if num_neighboring_electrons == 1 or num_neighboring_electrons == 2:
+                new_heads.add(c)
+            else:
+                new_conductors.add(c)
+
+        self.__electron_tail_positions = new_tails
+        self.__electron_head_positions = new_heads
+        self.__conductor_positions = new_conductors
--- tests/test_wireworld.py
+++ tests/test_wireworld.py
@@ -1,15 +1,71 @@
+import pytest
 from models import Cell
 from models.rules import WireWorld
 
 
+def test_there_can_be_no_cell_overlap():
+    with pytest.raises(ValueError):
+        _ = WireWorld(
+            conductor_positions={(7, 9)},
+            electron_head_positions={(7, 8), (7, 9), (7, 10)},
+        )
+
+
 def test_dead_world_stays_dead():
-    wireworld = WireWorld(initial_cells=set())
+    wireworld = WireWorld()
     wireworld.tick()
     assert len(wireworld.alive_cells) == 0
 
 
 def test_electron_head_becomes_electron_tail():
-    electron = Cell(10, 12, state=2)
-    wireworld = WireWorld(initial_cells={electron})
+    wireworld = WireWorld(electron_head_positions={(10, 12)})
     wireworld.tick()
     assert wireworld.alive_cells == {Cell(10, 12, state=3)}
+
+
+def test_electron_tail_becomes_conductor():
+    wireworld = WireWorld(electron_tail_positions={(7, 9)})
+    wireworld.tick()
+    assert wireworld.alive_cells == {Cell(7, 9, state=1)}
+
+
+def test_conductor_with_no_neighboring_electrons_stays_conductor():
+    wireworld = WireWorld(conductor_positions={(7, 9)})
+    wireworld.tick()
+    assert wireworld.alive_cells == {Cell(7, 9, state=1)}
+
+
+def test_conductor_with_more_than_two_neighboring_electrons_stays_conductor():
+    electrons = {(7, 8), (7, 10), (8, 9)}
+    wireworld = WireWorld(
+        conductor_positions={(7, 9)}, electron_head_positions=electrons
+    )
+    wireworld.tick()
+    assert wireworld.alive_cells == {
+        Cell(7, 9, state=1),
+        Cell(7, 8, state=3),
+        Cell(7, 10, state=3),
+        Cell(8, 9, state=3),
+    }
+
+
+def test_conductor_with_one_neighboring_electron_becomes_electron():
+    electrons = {(7, 8)}
+    wireworld = WireWorld(
+        conductor_positions={(7, 9)}, electron_head_positions=electrons
+    )
+    wireworld.tick()
+    assert wireworld.alive_cells == {Cell(7, 9, state=2), Cell(7, 8, state=3)}
+
+
+def test_conductor_with_two_neighboring_electrons_becomes_electron():
+    electrons = {(7, 8), (7, 10)}
+    wireworld = WireWorld(
+        conductor_positions={(7, 9)}, electron_head_positions=electrons
+    )
+    wireworld.tick()
+    assert wireworld.alive_cells == {
+        Cell(7, 9, state=2),
+        Cell(7, 8, state=3),
+        Cell(7, 10, state=3),
+    }
