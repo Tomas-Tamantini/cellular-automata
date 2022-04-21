dc8b41c7c78a1aa809783f180314d049536c2fb1
Tomas Tamantini <tomasbft@gmail.com>
2022-04-21 09:11:24 -0300

    Test Seeds cell birth rule

--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -1,22 +1,34 @@
+from models.cell import Cell
 from models.rules import Seeds
 
 
 def test_seeds_has_only_two_states():
-    assert Seeds(cells=set()).number_of_cell_states == 2
+    assert Seeds(initial_cell_positions=set()).number_of_cell_states == 2
 
 
 def test_world_has_initial_living_cells():
-    seeds = Seeds(cells={(0, 0), (10, 20)})
+    seeds = Seeds(initial_cell_positions={(0, 0), (10, 20)})
     assert len(seeds.alive_cells) == 2
 
 
 def test_dead_world_stays_dead():
-    seeds = Seeds(cells=set())
+    seeds = Seeds(initial_cell_positions=set())
     seeds.tick()
     assert len(seeds.alive_cells) == 0
 
 
 def test_alive_cells_die():
-    seeds = Seeds(cells={(0, 0), (10, 20)})
+    seeds = Seeds(initial_cell_positions={(0, 0), (10, 20)})
     seeds.tick()
     assert len(seeds.alive_cells) == 0
+
+
+def test_dead_cell_with_two_neighbors_is_born():
+    seeds = Seeds(initial_cell_positions={(10, 10), (11, 10), (10, 11)})
+    seeds.tick()
+    assert seeds.alive_cells == {
+        Cell(9, 10),
+        Cell(9, 11),
+        Cell(10, 9),
+        Cell(11, 9),
+    }
