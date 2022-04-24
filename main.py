860d30c59d40285d400aa35c5a04b086d4215fb1
Tomas Tamantini <tomasbft@gmail.com>
2022-04-24 12:43:54 -0300

    Sort imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.rules import Seeds, WireWorld
-from ui import run_wireworld_example, run_seeds_example,run_game_of_life_example
+from ui import run_game_of_life_example, run_seeds_example, run_wireworld_example
 
 if __name__ == "__main__":
     # run_game_of_life_example()
--- ui/__init__.py
+++ ui/__init__.py
@@ -1,3 +1,7 @@
 from .lattice import Lattice
 from .tk_animation import run_tkinter_animation
-from .tk_examples import run_seeds_example, run_wireworld_example, run_game_of_life_example
+from .tk_examples import (
+    run_game_of_life_example,
+    run_seeds_example,
+    run_wireworld_example,
+)
